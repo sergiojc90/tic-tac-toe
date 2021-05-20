f2473aa8f369c2a1b8bedf1edb6153d9ab4246ee
sergiojc90 <sergiojc90@gmail.com>
2021-05-19 23:39:57 -0500

    Creating factoru functions

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Factory function for creating the gameboard
+function createGameboard(){
+    let board =[];
+
+    return board;
+}
+
+// Factory function for creating the players
+function createPlayer(name){
+    return {name};
+}
+
+// Factory function for controling the game flow
+function gameFlow(){
+
+}
