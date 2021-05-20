2d83ae264d3bf54f2126d510fdf9ab0522a8bb85
sergiojc90 <sergiojc90@gmail.com>
2021-05-20 00:01:39 -0500

    Changing the module for a function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// Module for creating the gameboard
-    (function createGameboard(){
-        let board =[];
+// Function for creating the gameboard
+    function createGameboard(){
+        let board = new Array(9);
 
         return board;
-    })();
+    };
 
 // Factory function for creating an objecto that controls the game flow
     function gameFlow(){
