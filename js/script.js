1f4d1d3a43af27051912bd8705036806389273c1
sergiojc90 <sergiojc90@gmail.com>
2021-05-19 23:43:53 -0500

    Changing the createGameboard to a module)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-// Factory function for creating the gameboard
-function createGameboard(){
-    let board =[];
+// Module for creating the gameboard
+    (function createGameboard(){
+        let board =[];
 
-    return board;
-}
+        return board;
+    })();
+
+// Factory function for creating an objecto that controls the game flow
+    function gameFlow(){
+
+    };
 
 // Factory function for creating the players
-function createPlayer(name){
-    return {name};
-}
-
-// Factory function for controling the game flow
-function gameFlow(){
-
-}
+    function createPlayer(name){
+        return {name};
+    }
