65df4591a82ee9493fb2ff7a1de85d9b143d4a72
sergiojc90 <sergiojc90@gmail.com>
2021-05-21 23:14:49 -0500

    Indexing each element of the array display on the grid

--- js/script.js
+++ js/script.js
@@ -8,16 +8,17 @@
 
         gameBoard = document.getElementById("gameBoard");
 
+        let index = 0;
         board.forEach(element => {
+
             square = document.createElement("div");
+            square.dataset.index = index;
+            index += 1;
             square.classList.add("square");
             square.textContent = element;
 
-
             gameBoard.appendChild(square);
         })
-        
-        
         return board;
     };
 
