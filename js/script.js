e09a4e5b9f67b1dcd573cf32f5e120d6e62e4cf4
sergiojc90 <sergiojc90@gmail.com>
2021-05-21 22:39:02 -0500

    Testing the display of the array

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 // Function for creating the gameboard
     function createGameboard(){
         let board = [
-            ["X","O","X"],
-            ["X","O","X"],
-            ["X","O","X"]
+            "X","O","X",
+            "X","O","X",
+            "X","O","X"
         ];
 
+        gameBoard = document.getElementById("gameBoard");
+
+        board.forEach(element => {
+            square = document.createElement("div");
+            square.classList.add("square");
+            square.textContent = element;
+
+
+            gameBoard.appendChild(square);
+        })
+        
+        
         return board;
     };
+
+    createGameboard();
 
 // Factory function for creating an objecto that controls the game flow
     function gameFlow(){
