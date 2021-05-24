37ab1140c00b009741f8a81361d8d089033c0822
sergiojc90 <sergiojc90@gmail.com>
2021-05-23 22:09:33 -0500

    Starting again using two modules, one for the initial window and one for the actual game

--- js/script.js
+++ js/script.js
@@ -1,46 +1,4 @@
 // Board Module
-    (function(){
-        let gameBoard = {
-            board: [
-                "","","",
-                "","","",
-                "","","",
-            ],
-            init: function() {
-                this.cacheDom();
-                this.bindEvents();
-            },
-            cacheDom: function() {
-                this.gameBoard = document.getElementById("gameBoard");
-                this.startButton = document.getElementById("startButton");
-            },
-            bindEvents: function() {
-                this.startButton.addEventListener("click",this.startGame.bind(this));
-            },
-            render: function() {
-                this.clearGrid();
-                this.board.forEach(this.addSquare)
-            },
-            addSquare: function(item){
-                square = document.createElement("div");
-                square.classList.add("square");
-                square.textContent = item;
-                gameBoard.gameBoard.appendChild(square);
-            },
-            addSelection:function(e){
-                console.log(e.target.dataset.index);
-            },
-            startGame:function(){
-                this.startButton.classList.add("btn--disable");
-                this.render();
-            },
-            clearGrid: function(){
-                while(this.gameBoard.hasChildNodes()){
-                    this.gameBoard.removeChild(this.gameBoard.lastChild);
-                }
-            }
-        }
-
-        gameBoard.init();
-    })()
-
+(function(){
+    let 
+})();
