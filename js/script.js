f665fe37228b2632222f064ded46ffd322c1f3e2
sergiojc90 <sergiojc90@gmail.com>
2021-05-24 14:12:16 -0500

    changing styles and js code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 // Board Module
-    const gameBoard = (function(){
-        
-    })();
+    let board = new Array(9).fill(null);
 
+    // DOM manipulation
+    let gameboard = document.getElementById("gameBoard");
+    let startButton = document.getElementById("startButton");
+    let reset = document.getElementById("reset");
+    let player = document.getElementById("player");
+    
+    render();
+
+    function render(){
+        board.forEach(addSquares);
+    };
+
+    function addSquares(element,index){
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.dataset.index = index;
+        square.textContent = element;
+        gameboard.appendChild(square);
+    };
+
+    let squareOptions = document.querySelectorAll(".square");
+    squareOptions.forEach(addPlayerSelection);
+
+    function addPlayerSelection(squareOption){
+        squareOption.addEventListener("click",function(event){
+            console.log(event.target.dataset.index);
+        })
+    }
+
+    function playerSelection(event){
+        if(event.target.textContent === null){
+            event.textContent = player.textContent;
+            playerTurn();
+        }
+    }
+
+    function playerTurn(){
+        
+    }
