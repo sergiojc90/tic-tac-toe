51a5cfc75b801d63999631be172be37190273295
sergiojc90 <sergiojc90@gmail.com>
2021-05-25 02:37:30 -0500

    Changing all code in to a module

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@
 
     const gamePlay = (function(){
         // DOM manipulation
+        let board = new Array(9).fill(null);
+
         let gameboard = document.getElementById("gameBoard");
         let startButton = document.getElementById("startButton");
         let resetButton = document.getElementById("reset");
@@ -31,6 +33,10 @@
             gameboard.appendChild(square);
         };
 
+        function addPlayerSelection(squareOption){
+            squareOption.addEventListener("click",playerSelection)
+        }
+    
         function playerSelection(event){
             if(event.target.textContent === ""){
                 event.target.textContent = player.textContent;
@@ -38,83 +44,45 @@
                 playerTurn();
             }
         }
-    })();
 
-    let board = new Array(9).fill(null);
-
-    // DOM manipulation
-    let gameboard = document.getElementById("gameBoard");
-    let startButton = document.getElementById("startButton");
-    let resetButton = document.getElementById("reset");
-    let player = document.getElementById("player");
-    
-    resetButton.addEventListener("click", reset);
-
-    render();
-
-    function render(){
-        board.forEach(addSquares);
-    };
-
-    function addSquares(element,index){
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.dataset.index = index;
-        square.textContent = element;
-        gameboard.appendChild(square);
-    };
-
-    let squareOptions = document.querySelectorAll(".square");
-    squareOptions.forEach(addPlayerSelection);
-
-    function addPlayerSelection(squareOption){
-        squareOption.addEventListener("click",playerSelection)
-    }
-
-    function playerSelection(event){
-        if(event.target.textContent === ""){
-            event.target.textContent = player.textContent;
-            board[event.target.dataset.index] = event.target.textContent;
-            playerTurn();
-        }
-    }
-
-    function playerTurn(){
-        if (player.textContent === "X"){
-            player.textContent="O";
-        }else{
-            player.textContent="X";
-        }
-    }
-
-    function findWinningCombination(){
-        const winningCombinations = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6],
-        ]
-
-        for(const combination of winningCombinations){
-            const[a,b,c] = combination;
-
-            if(board[a] && (board[a] === board[b] && board[a] === board[c])){
-                return combination;
+        function playerTurn(){
+            if (player.textContent === "X"){
+                player.textContent="O";
+            }else{
+                player.textContent="X";
             }
-
-            return null;
         }
-    }
-
-    function reset(){
-        board = new Array(9).fill(null);
-        while(gameboard.hasChildNodes()){
-            console.log(gameboard.hasChildNodes())
-            gameboard.removeChild(gameboard.firstChild);
+    
+        function findWinningCombination(){
+            const winningCombinations = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6],
+            ]
+    
+            for(const combination of winningCombinations){
+                const[a,b,c] = combination;
+    
+                if(board[a] && (board[a] === board[b] && board[a] === board[c])){
+                    return combination;
+                }
+    
+                return null;
+            }
         }
-        render();
-    }
+
+        function reset(){
+            board = new Array(9).fill(null);
+            while(gameboard.hasChildNodes()){
+                console.log(gameboard.hasChildNodes())
+                gameboard.removeChild(gameboard.firstChild);
+            }
+            render();
+        }
+    
+    })();
