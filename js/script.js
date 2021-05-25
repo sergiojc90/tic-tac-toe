7f54ee6630db9b8902b6d6455b78dd4b28454754
sergiojc90 <sergiojc90@gmail.com>
2021-05-25 01:38:32 -0500

    Adding a function to check if there is a winning combination

--- js/script.js
+++ js/script.js
@@ -29,9 +29,9 @@
     }
 
     function playerSelection(event){
-        console.log(event.target.value);
         if(event.target.textContent === ""){
             event.target.textContent = player.textContent;
+            board[event.target.dataset.index] = event.target.textContent;
             playerTurn();
         }
     }
@@ -41,5 +41,28 @@
             player.textContent="O";
         }else{
             player.textContent="X";
+        }
+    }
+
+    function findWinningCombination(){
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ]
+
+        for(const combination of winningCombinations){
+            const[a,b,c] = combination;
+
+            if(board[a] && (board[a] === board[b] && board[a] === board[c])){
+                return combination;
+            }
+
+            return null;
         }
     }
